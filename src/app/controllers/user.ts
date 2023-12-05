b6b6d2a47dbde4a05aed590b202717e9c22b1c0d
Gabriel Apolinario <vieira.gah@outlook.com>
2023-12-05 00:00:31 -0300

    feat: added login / checkToken

--- src/app/controllers/user.ts
+++ src/app/controllers/user.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from 'express';
-import { hash } from 'bcryptjs';
+import { NextFunction, Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import { Prisma } from '../../infra';
-
+import jwt from 'jsonwebtoken';
 export class User {
   async create(req: Request, res: Response): Promise<Response> {
     const { name, email, password, office } = req.body;
 
-    if (password.lenght < 6) {
+    if (password.length < 6) {
       return res.status(400).json('Senha deve conter no mínimo 6 caracteres.');
     }
     const existingEmail = await Prisma.users.findFirst({
@@ -17,7 +17,7 @@ export class User {
     if (existingEmail) {
       return res.status(400).json('E-mail já cadastrado.');
     }
-    const hashPassword = await hash(password, 10);
+    const hashPassword = await bcrypt.hash(password, 10);
     const createUser = await Prisma.users.create({
       data: {
         email,
@@ -87,6 +87,75 @@ export class User {
     } catch (error) {
       console.log(error);
       return res.status(500).json('Erro ao excluir usuário');
+    }
+  }
+  async login(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json('Insira e-mail e senha.');
+    }
+
+    const user = await Prisma.users.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json('Usuário não encontrado');
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      return res.status(400).json('Senha incorreta.');
+    }
+
+    try {
+      const secret = process.env.SECRET;
+      if (!secret) {
+        throw new Error('Chave secreta não configurada.');
+      }
+
+      const token = jwt.sign(
+        {
+          id: user.id,
+        },
+        secret,
+      );
+
+      return res.status(200).json({ user, token });
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json('Algo deu errado, tente novamente mais tarde.');
+    }
+  }
+
+  async checkToken(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response> {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+      return res.status(401).json('Acesso negado!');
+    }
+    try {
+      const secret = process.env.SECRET;
+      if (!secret) {
+        throw new Error('A chave secreta não está configurada.');
+      }
+      jwt.verify(token, secret);
+      next();
+      return res.status(200).json('Autenticado com sucesso.');
+    } catch (error) {
+      return res
+        .status(500)
+        .json('Algo deu errado, tente novamente mais tarde');
     }
   }
 }
